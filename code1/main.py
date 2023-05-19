e8fe9a9abdc8c9aa8e4ed37af84a9d4841f8aa9f
Khom <mgrprinc37@gmail.com>
2023-05-19 09:39:57 +0545

    ikhom: template matching works

--- code1/main.py
+++ code1/main.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import numpy as np
 
+# img = cv.imread('honkai_field_right.jpg', cv.IMREAD_UNCHANGED)
+# pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_UNCHANGED)
+
 img = cv.imread('honkai_field_right.jpg', cv.IMREAD_REDUCED_COLOR_2)
 pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_REDUCED_COLOR_2)
 
@@ -9,6 +12,7 @@ pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_REDUCED_COLOR_2)
 
 result = cv.matchTemplate(img, pointer_img, cv.TM_CCOEFF_NORMED)
 
+# get the best match positions
 min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)  # 0=blackest value, 1=brightest white
 
 print('Best match top left position: %s' % str(max_loc))
@@ -17,6 +21,21 @@ print('Best match confidence: %s' % max_val)
 threshold = 0.7
 if max_val >= threshold:
     print('found pointer')
+
+    # get the dimensions of the pointer image
+    pointer_w = pointer_img.shape[1]
+    pointer_h = pointer_img.shape[0]
+
+    top_left = max_loc
+    bottom_right = (top_left[0] + pointer_w, top_left[1] + pointer_h)
+
+    cv.rectangle(img, top_left, bottom_right,
+                 color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
+    
+    cv.imshow('Result', img)
+    cv.waitKey()
+
+
 else:
     print('Pointer not found')
 
