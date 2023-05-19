71b0e61e0dbf0f57b794e1fbd35528a02e25aaaf
Khom <mgrprinc37@gmail.com>
2023-05-19 08:41:26 +0545

    ikhom: template matching with opencv python

--- code1/main.py
+++ code1/main.py
@@ -1,9 +1,10 @@
-import cv2 as cv2
+import cv2 as cv
 import numpy as np
 
 img = cv.imread('honkai_field_right.jpg', cv.IMREAD_UNCHANGED)
-pointer_img = cv.imread('honkai_goal_right.jpg', cv.IMREAD_UNCHANGED)
+pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(img, pointer_img, cv.TM_CCOEFF_NORMED)
 
 cv.imshow('Result', result)
+cv.waitKey()
