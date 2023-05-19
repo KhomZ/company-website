2bc4399a350beb67fc50cbd9f6fc32f4504ac58d
Khom <mgrprinc37@gmail.com>
2023-05-19 09:17:52 +0545

    ikhom: Best match top left position: (815, 225) Best match confidence: 0.7663276791572571

--- code1/main.py
+++ code1/main.py
@@ -1,22 +1,27 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('honkai_field_right.jpg', cv.IMREAD_UNCHANGED)
-pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_UNCHANGED)
+img = cv.imread('honkai_field_right.jpg', cv.IMREAD_REDUCED_COLOR_2)
+pointer_img = cv.imread('goal_right.jpg', cv.IMREAD_REDUCED_COLOR_2)
+
+# img = cv.imread('honkai_field_left.jpg', cv.IMREAD_UNCHANGED)
+# pointer_img = cv.imread('goal_left.jpg', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(img, pointer_img, cv.TM_CCOEFF_NORMED)
 
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)  # 0=blackest value, 1=brightest white
 
 print('Best match top left position: %s' % str(max_loc))
 print('Best match confidence: %s' % max_val)
 
-threshold = 0.8
+threshold = 0.7
 if max_val >= threshold:
     print('found pointer')
 else:
     print('Pointer not found')
 
 
+
+# debug code
 # cv.imshow('Result', result)
 # cv.waitKey()
